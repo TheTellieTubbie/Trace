e4b457068745700f9a251c8897647aba01b0e775
TheTellieTubbie <gfb448@student.bham.ac.uk>
2025-07-09 19:28:57 +0100

    Initial commit

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,28 @@
 import os
 from flask import Blueprint, render_template, request
 from werkzeug.utils import secure_filename
+from .extractor import extract_chunks
 
 main = Blueprint('main', __name__)
 
 
 @main.route("/", methods=["GET", "POST"])
 def index():
+    extracted_data = {}
+
     if request.method == "POST":
         uploaded_files = request.files.getlist("files")
         saved_paths = []
+
         for file in uploaded_files:
             if file.filename != "":
                 filename = secure_filename(file.filename)
                 save_path = os.path.join("uploads", filename)
                 file.save(save_path)
                 saved_paths.append(filename)
+
+                chunks = extract_chunks(save_path)
+                extracted_data[filename] = chunks
 
 
         return render_template("index.html", uploaded=True, files=saved_paths)
